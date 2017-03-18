a2f727b7e51bd0d3e496635fc51da95fa2e33a22
Boy Codeing <CodeingBoy@gmail.com>
2017-03-18 15:14:14 +0800

    Add Floyd-Warshall

---
+++ Floyd-Warshall/Floyd-Warshall.c
@@ -0,0 +1,52 @@
+// Floyd-Warshall
+// Language: C99
+
+#include <stdio.h>
+
+#define SIZE  4
+#define MAX_DIST 999999
+
+int graph[SIZE][SIZE];
+
+void print_graph(){
+    for(int i = 0;i < SIZE;i++){
+        for(int j = 0;j < SIZE;j++){
+            printf("%d\t", graph[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void Floyd_Warshall(){
+    for(int middle_index = 0;middle_index < SIZE;middle_index++){
+        for(int a_index = 0;a_index < SIZE;a_index++){
+            for(int b_index = 0;b_index < SIZE;b_index++){
+                if(graph[a_index][b_index] > graph[a_index][middle_index] + graph[middle_index][b_index]){
+                    graph[a_index][b_index] = graph[a_index][middle_index] + graph[middle_index][b_index];
+                }
+            }
+        }
+#ifdef VERBOSE
+        printf("After %d:\n", middle_index);
+        print_graph();
+        printf("\n");
+#endif
+    }
+}
+
+int main(){
+    for(int i = 0;i < SIZE;i++){
+        for(int j = 0;j < SIZE;j++){
+            scanf("%d", &graph[i][j]);
+            if(graph[i][j] == -1){
+                graph[i][j] = MAX_DIST;
+            }
+        }
+    }
+
+    Floyd_Warshall();
+    print_graph();
+    getchar();
+
+    return 0;
+}
